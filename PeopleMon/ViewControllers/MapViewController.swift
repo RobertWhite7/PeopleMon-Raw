6b925a6dad9229c1860159a98c56c80b0b52c5c3
RobertWhite7 <robertwhite@robertwhitembp.local>
2016-11-11 16:24:16 -0500

    Worked on some things in mapViewController

--- PeopleMon/ViewControllers/MapViewController.swift
+++ PeopleMon/ViewControllers/MapViewController.swift
@@ -6,15 +6,40 @@
 //  Copyright © 2016 Teky. All rights reserved.
 //
 
-import Foundation
 import UIKit
 import MapKit
-/*
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var theMap: MKMapView!
     
+    var locationManager: CLLocationManager!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if (CLLocationManager.locationServicesEnabled()){
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    private func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = locations.last as! CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.theMap.setRegion(region, animated: true)
+    }
+}
+/*
+
+class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var listDirectionsButton: UIBarButtonItem!
+ 
     
     
     let locationManager = CLLocationManager()
@@ -26,8 +51,7 @@ class MapViewController: UIViewController {
     let searchController = UISearchController(searchResultsController: nil)
     var searchResults: [MKMapItem] = []
     
-    var annotations: [MapPin] = []
-    
+ 
     let transitionManager = TransitionManager()
     
     var overlay: MKOverlay?
@@ -49,13 +73,7 @@ class MapViewController: UIViewController {
         mapView.showsUserLocation = true
         locationManager.startUpdatingLocation()
         
-        searchController.searchResultsUpdater = self
-        searchController.hidesNavigationBarDuringPresentation = false
-        searchController.dimsBackgroundDuringPresentation = false
-        searchController.searchBar.sizeToFit()
-        searchController.searchBar.delegate = self
-        searchController.searchBar.placeholder = "Search Locations"
-        self.navigationItem.titleView = searchController.searchBar
+ 
         
         tableView.isHidden = true
         
@@ -64,8 +82,7 @@ class MapViewController: UIViewController {
         
         navigationController?.delegate = self
         
-        setupDirectionsView()
-        showDirectionsListButton(false)
+    
     }
     
     override func didReceiveMemoryWarning() {
@@ -81,15 +98,7 @@ class MapViewController: UIViewController {
         }
     }
     
-    @IBAction func showDirectionsList(_ sender: AnyObject) {
-        showingDirections = true
-        directionsTableView.reloadData()
-        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: [], animations: {
-            self.directionsView.alpha = 1.0
-            self.directionsView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0).concatenating(CGAffineTransform(translationX: 0, y: 0))
-        }, completion: nil)
-        
-    }
+
     
     @IBAction func hideDirectionsList(_ sender: AnyObject) {
         UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: [], animations: {
@@ -102,130 +111,13 @@ class MapViewController: UIViewController {
     
     
     // MARK: - Private Functions
-    fileprivate func searchPlaces(_ searchText: String, plotOnMap: Bool) {
-        let request = MKLocalSearchRequest()
-        request.naturalLanguageQuery = searchText
-        request.region = mapView.region
-        
-        if  let search = search, search.isSearching {
-            search.cancel()
-        }
-        
-        search = MKLocalSearch(request: request)
-        search.start { (response, error) in
-            if let response = response {
-                self.searchResults = []
-                for item in response.mapItems {
-                    print(item.name!)
-                    self.searchResults.append(item)
-                }
-                
-                if plotOnMap {
-                    self.plotLocations()
-                }
-            }
-            
-            self.tableView.reloadData()
-        }
-    }
+
+
     
-    fileprivate func addressString(_ place: MKMapItem) -> String {
-        let address = place.placemark.addressDictionary!
-        var addressString = ""
-        if let formatted = address["FormattedAddressLines"] as? [String] {
-            addressString = formatted.joined(separator: ", ")
-        }
-        return addressString
-    }
+ 
+
     
-    fileprivate func plotLocations() {
-        mapView.removeAnnotations(annotations)
-        annotations = []
-        for place in searchResults {
-            //            let annotation = MKPointAnnotation()
-            //            annotation.coordinate = place.placemark.coordinate
-            //            annotation.title = place.name
-            //            annotation.subtitle = addressString(place)
-            let annotation = MapPin(coordinate: place.placemark.coordinate, title: place.name, address: addressString(place), phone: place.phoneNumber, url: place.url)
-            
-            
-            annotations.append(annotation)
-            mapView.addAnnotation(annotation)
-        }
-        if !searchResults.isEmpty {
-            mapView.centerCoordinate = searchResults.first!.placemark.coordinate
-        }
-    }
-    
-    fileprivate func plotRoute(_ destination: CLLocationCoordinate2D){
-        if let overlay = overlay {
-            mapView.remove(overlay)
-        }
-        if let route = route {
-            let polyline = route.polyline
-            mapView.add(polyline, level: .aboveRoads)
-            mapView.setVisibleMapRect(polyline.boundingMapRect, edgePadding: UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20), animated: true)
-            
-            showDirectionsListButton(true)
-        }
-    }
-    fileprivate func setupDirectionsView() {
-        let bounds = UIScreen.main.bounds
-        let navbarHeight: CGFloat = 64
-        let toolbarHeight: CGFloat = 44
-        let padding: CGFloat = 20
-        let buttonSize: CGFloat = 32
-        let directionsViewFrame = CGRect(x: padding,
-                                         y: navbarHeight + padding,
-                                         width: bounds.width - (padding * 2),
-                                         height: bounds.height - navbarHeight - toolbarHeight - (padding * 2))
-        let directionsTableFrame = CGRect(x: 0,
-                                          y: buttonSize,
-                                          width: directionsViewFrame.size.width,
-                                          height: directionsViewFrame.size.height - buttonSize)
-        
-        let closeButton = UIButton(frame: CGRect(x: 0, y: 0,
-                                                 width: directionsViewFrame.width, height: buttonSize))
-        
-        closeButton.setBackgroundImage(#imageLiteral(resourceName: "CloseButton"), for: UIControlState())
-        closeButton.addTarget(self, action: #selector(hideDirectionsList(_:)), for: .touchUpInside)
-        
-        directionsView = UIView(frame: directionsViewFrame)
-        directionsView.backgroundColor = UIColor.white
-        
-        directionsTableView = UITableView(frame: directionsTableFrame)
-        directionsTableView.dataSource = self
-        directionsTableView.delegate = self
-        directionsTableView.rowHeight = UITableViewAutomaticDimension
-        directionsTableView.estimatedRowHeight = 44
-        
-        directionsView.layer.cornerRadius = 8
-        directionsView.clipsToBounds = true
-        directionsView.layer.masksToBounds = true
-        directionsView.layer.shadowColor = UIColor.black.cgColor
-        directionsView.layer.shadowOffset = CGSize(width: 0, height: 5)
-        directionsView.layer.shadowOpacity = 0.3
-        directionsView.layer.shadowRadius = 10.0
-        directionsView.alpha = 0.0
-        let scaleTransform = CGAffineTransform(scaleX: 0.8, y: 0.8)
-        let translateTransform = CGAffineTransform(translationX: 0, y: bounds.height)
-        directionsView.transform = scaleTransform.concatenating(translateTransform)
-        
-        directionsView.addSubview(directionsTableView)
-        directionsView.addSubview(closeButton)
-        
-        view.addSubview(directionsView)
-    }
-    
-    fileprivate func showDirectionsListButton(_ show: Bool){
-        if show {
-            listDirectionsButton.title = "List Directions"
-            listDirectionsButton.isEnabled = true
-        }else{
-            listDirectionsButton.title = ""
-            listDirectionsButton.isEnabled = false
-        }
-    }
+
 }
 
 
@@ -242,61 +134,7 @@ extension MapViewController: CLLocationManagerDelegate {
 }
 
 
-// MARK: - UISearchResultsUpdating
-extension MapViewController: UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        if let searchText = searchController.searchBar.text, searchText != "" {
-            searchPlaces(searchText, plotOnMap: false)
-        } else {
-            searchResults = []
-            if let search = search, search.isSearching {
-                search.cancel()
-            }
-            
-            tableView.reloadData()
-        }
-    }
-}
 
-
-// MARK: - UISearchBarDelegate
-extension MapViewController: UISearchBarDelegate {
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        if let searchText = searchBar.text, searchText != "" {
-            searchPlaces(searchText, plotOnMap: true)
-        }
-    }
-    
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        tableView.isHidden = false
-        
-        showDirectionsListButton(false)
-        hideDirectionsList(self)
-        
-        if let overlay = overlay{
-            mapView.remove(overlay)
-        }
-    }
-    
-    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        tableView.isHidden = true
-    }
-    
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        
-        
-        
-        
-        searchResults = []
-        tableView.reloadData()
-        
-        mapView.removeAnnotations(annotations)
-        annotations = []
-    }
-    
-    
-    
-}
 
 
 // MARK: - UITableView Delegate/Datasource
@@ -326,7 +164,7 @@ extension MapViewController: UITableViewDelegate, UITableViewDataSource {
             
             
             cell.textLabel?.text = place.name
-            cell.detailTextLabel?.text = addressString(place)
+            
             
             return cell
         }
@@ -342,7 +180,6 @@ extension MapViewController: UITableViewDelegate, UITableViewDataSource {
                 search.cancel()
             }
             
-            plotLocations()
             searchController.searchBar.resignFirstResponder()
             mapView.selectAnnotation(annotations.first!, animated: false)
             tableView.isHidden = true
@@ -412,14 +249,14 @@ extension MapViewController: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         if control.tag == 0 {
-            if let mapPin = view.annotation as? MapPin {
+         /*  if let mapPin = view.annotation as? MapPin {
                 let mapDetailVC = storyboard!.instantiateViewController(withIdentifier: String(describing: MapDetailViewController.self)) as! MapDetailViewController
                 
                 mapDetailVC.region = mapView.region
                 
                 mapDetailVC.mapPin = mapPin
                 navigationController?.pushViewController(mapDetailVC, animated: true)
-            }
+            } */
         }else if control.tag == 1 {
             if let coordinate = view.annotation?.coordinate {
                 let request = MKDirectionsRequest()
@@ -432,8 +269,7 @@ extension MapViewController: MKMapViewDelegate {
                     guard let response = respones else{
                         return
                     }
-                    self.route = response.routes.first
-                    self.plotRoute(coordinate)
+                 
                 })
             }
         }
@@ -455,5 +291,34 @@ extension MapViewController: UINavigationControllerDelegate {
     func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationControllerOperation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         transitionManager.reverse = operation == .pop
         return transitionManager
+    }
+} */
+/*
+
+class MapViewController : UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+    var locationManager: CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+        
+    }
+    private func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = locations.last as! CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.map.setRegion(region, animated: true)
     }
 }*/
