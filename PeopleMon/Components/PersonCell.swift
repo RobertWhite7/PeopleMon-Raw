655929e2d77228f9a7932dc054e5933637d8a28a
RobertWhite7 <robertwhite@robertwhitembp.local>
2016-11-14 16:07:27 -0500

    finished final product

--- PeopleMon/Components/PersonCell.swift
+++ PeopleMon/Components/PersonCell.swift
@@ -10,26 +10,23 @@ import UIKit
 import AFDateHelper
 
 class PersonCell: UITableViewCell {
-    @IBOutlet weak var avatar: CircleImage!
+
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
+   
     
     var person: Account!
     
     func setupCell(person: Account) {
         self.person = person
-        
         nameLabel.text = person.username
-        if let createdDate = person.created {
-            let date = Date(fromString: createdDate, format: .iso8601(nil))
-            dateLabel.text = date.toString(.custom("M/d/yyyy h:m:s a"))
-        }
         
-        if let image = Utils.imageFromString(imageString: person.avatar) {
+    
+        
+    /*    if let image = Utils.imageFromString(imageString: person.avatar) {
             avatar.image = image
         } else {
-            avatar.image = Images.Avatar.image()
+            avatar.image = #imageLiteral(resourceName: "Defaultimage")
         }
-        avatar.setupView(size: 60)
+  */
     }
 }
